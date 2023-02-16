97b042169f85ba49ec19247e2267cc01edd88e47
PatrykP <114055028+PatrykPias@users.noreply.github.com>
2023-02-16 13:43:41 +0100

    Add files via upload

---
+++ web_pedidosMVC/controllers/sesion.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_POST["insert"])) {
+    session_start();
+    
+}
+if (isset($_SESSION["user"])) {
+    $_SESSION = array();
+
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    session_destroy();
+}
+?>
--- web_pedidosMVC/index.php
+++ web_pedidosMVC/index.php
@@ -1,23 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <?php
-if (isset($_POST["insert"])) {
-    session_start();
-    
-}
-if (isset($_SESSION["user"])) {
-    $_SESSION = array();
-
-    if (ini_get("session.use_cookies")) {
-        $params = session_get_cookie_params();
-        setcookie(session_name(), '', time() - 42000,
-            $params["path"], $params["domain"],
-            $params["secure"], $params["httponly"]
-        );
-    }
-
-    session_destroy();
-}
+    require_once("controllers/sesion.php");
 ?>
 <head>
     <meta charset="UTF-8">
